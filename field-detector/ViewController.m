225dfb5359769ee0dbcad956221fa03bc04c19cd
antonino-u <ant@urbano.me>
2012-11-16 16:02:08 -0500

    OpenCV setup

--- field-detector/ViewController.m
+++ field-detector/ViewController.m
@@ -18,6 +18,8 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+	UIAlertView * alert = [[UIAlertView alloc] initWithTitle:@"Hello!" message:@"Welcome to OpenCV" delegate:self cancelButtonTitle:@"Continue" otherButtonTitles:nil];
+	[alert show];
 }
 
 - (void)didReceiveMemoryWarning
